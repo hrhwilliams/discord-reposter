b700ea6c75015af5c426fee8479fb7e0eaea12b3
Harlan Williams <hrw@ku.edu>
2024-10-06 19:01:50 -0500

    work on parser for other HTML log format

--- reposter/load.py
+++ reposter/load.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from bs4 import BeautifulSoup
 
 
 current_file = None
@@ -16,6 +17,14 @@ def set_current_file(attachment):
             return True
     return False
 
+
+def set_current_file2(attachment):
+    global current_file
+    response = requests.get(attachment.url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(str(response.content, encoding="utf-8"), 'html.parser')
+    # div class="chatlog__message-group"
 
 def get_current_file():
     global current_file
